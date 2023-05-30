c20fcb82f717a545458eee2d5b67b7325ee02d13
Joseph Stocks <jpstocks63@gmail.com>
2023-05-29 23:18:17 -0500

    Add styling to login/signup pages

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -3,7 +3,13 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <SignUp />
+      <SignUp
+        appearance={{
+          elements: {
+            logoBox: "flex items-center justify-center",
+          },
+        }}
+      />
     </main>
   );
 }

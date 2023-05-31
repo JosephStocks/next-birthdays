efd0f9d0a7e9d676296afac468e24e08f97ddac1
Joseph Stocks <jpstocks63@gmail.com>
2023-05-31 15:55:08 -0500

    Added react-tailwindcss-datepicker

--- src/pages/my-birthdays.tsx
+++ src/pages/my-birthdays.tsx
@@ -1,17 +1,39 @@
 import { type NextPage } from "next";
+import { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
 import { api } from "@/utils/api";
 import Layout from "@/components/Layout";
+import type { DateValueType } from "react-tailwindcss-datepicker/dist/types";
 
 const Home: NextPage = () => {
   const { data } = api.birthdays.getAll.useQuery();
+  const [value, setValue] = useState<DateValueType>({
+    startDate: new Date(),
+    endDate: null,
+  });
+
+  const handleValueChange = (newValue: DateValueType) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
 
   return (
     <Layout
       title="My Birthdays"
-      className="flex flex-col items-center justify-center"
+      className="flex max-w-screen-md flex-col items-center justify-center p-4"
     >
-      {data?.map((birthday) => (
-        <div key={birthday.id}>{birthday.firstName}</div>
+      <Datepicker
+        useRange={false}
+        asSingle={true}
+        value={value}
+        onChange={handleValueChange}
+      />
+      {data?.map(({ id, firstName, lastName, birthday }) => (
+        <div key={id}>
+          {firstName}
+          {lastName ? ` ${lastName}` : ""}
+          {JSON.stringify(birthday)}
+        </div>
       ))}
     </Layout>
   );

e2b85e6172b69cec965a7bd555592d0b50beb071
Joseph Stocks <jpstocks63@gmail.com>
2023-06-08 12:30:54 -0500

    Remove absolute positioning to fix bg svg image not showing during loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { BounceLoader } from "react-spinners";
 
 export const LoadingPage = () => {
   return (
-    <div className="absolute right-0 top-0 flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <BounceLoader color="#4375ae" />
       <span className="sr-only">Loading...</span>
     </div>
